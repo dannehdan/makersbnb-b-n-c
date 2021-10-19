f9b0245464f4308e71f7560bf0433bf75b2f40fa
nejla <nejlalowe@gmail.com>
2021-10-19 15:11:20 +0100

    Add method spec

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -1,4 +1,5 @@
 require "room"
+require "database_helpers"
 
 describe Room do
   describe "all" do
@@ -10,6 +11,17 @@ describe Room do
       expect(rooms[0].description).to eq "This is a room."
       expect(rooms[1].name).to eq "Room 2"
       expect(rooms[1].description).to eq "This is a room too."
+    end
+  end
+  describe "add" do
+    it "adds a new room" do
+      room = Room.add(name: "Room 1", description: "This is a room.")
+      persisted_data = persisted_data(id: room.id)
+
+      expect(room.name).to eq "Room 1"
+      expect(room.description).to eq "This is a room."
+      expect(room).to be_a Room
+      expect(room.id).to eq persisted_data["id"]
     end
   end
 end
