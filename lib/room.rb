bb1a2397ec943677216fadb5f3ba1a61aee8c167
Anmol <bhatianmol566@yahoo.co.uk>
2021-10-22 14:54:56 +0100

    Refactor to pass rubocop

--- lib/room.rb
+++ lib/room.rb
@@ -15,38 +15,24 @@ class Room
   def self.all
     result = Room.connection.exec("SELECT * FROM rooms;")
     result.map do |room|
-      Room.new(
-        id: room["id"], name: room["name"],
-        description: room["description"], rate: room["rate"],
-        available_from: room["available_from"], available_to: room["available_to"]
-      )
+      Room.new_with_hash(room)
     end
   end
 
   def self.add(name:, description:, rate:, available_from:, available_to:)
     result = Room.connection.exec_params(
       "INSERT INTO rooms (name, description, rate, available_from, available_to) 
-      VALUES($1, $2, $3, $4, $5) RETURNING id, name, description, rate, available_from, available_to;",
+      VALUES($1, $2, $3, $4, $5) 
+      RETURNING id, name, description, rate, available_from, available_to;",
       [name, description, rate, available_from, available_to]
     )
-    Room.new(
-      id: result[0]["id"], name: result[0]["name"],
-      description: result[0]["description"], rate: result[0]["rate"],
-      available_from: result[0]["available_from"], available_to: result[0]["available_to"]
-    )
+    Room.new_with_hash(result[0])
   end
 
   def self.find(id:)
     result = Room.connection.exec_params("SELECT * FROM rooms WHERE id = $1::int;", [id])
 
-    Room.new(
-      id: result[0]["id"],
-      name: result[0]["name"],
-      description: result[0]["description"],
-      rate: result[0]["rate"],
-      available_from: result[0]["available_from"],
-      available_to: result[0]["available_to"],
-    )
+    Room.new_with_hash(result[0])
   end
 
   def self.search(search)
@@ -54,12 +40,19 @@ class Room
       "SELECT * FROM rooms WHERE (name ILIKE $1) OR (description ILIKE $1);", ["%" + search + "%"]
     )
     result.map do |room|
-      Room.new(
-        id: room["id"], name: room["name"],
-        description: room["description"], rate: room["rate"],
-        available_from: room["available_from"], available_to: room["available_to"]
-      )
+      Room.new_with_hash(room)
     end
+  end
+
+  def self.new_with_hash(hash)
+    Room.new(
+      id: hash["id"],
+      name: hash["name"],
+      description: hash["description"],
+      rate: hash["rate"],
+      available_from: hash["available_from"],
+      available_to: hash["available_to"],
+    )
   end
 
   # :nocov:
